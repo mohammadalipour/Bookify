547e60ed131081046ae05c7073d1f9ac82052c67
Mohammad Alipour <alipour@billiger-mietwagen.de>
2024-05-07 17:34:05 +0200

    add pino-pretty

---
+++ apps/reservations/src/main.ts
@@ -0,0 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+
+import { ReservationsModule } from './reservations.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,7 +3,11 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
-import { ReservationDocument, ReservationSchema } from "./models/reservation.schema";
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -14,6 +18,16 @@ import { ReservationDocument, ReservationSchema } from "./models/reservation.sch
         schema: ReservationSchema,
       },
     ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
